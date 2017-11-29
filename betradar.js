4d54f5f35ef8ff882bca1918f727cbaab9d5693c
Felipe Figueroa <amenadiel@gmail.com>
2017-11-29 08:28:53 -0300

    betradar without async/await

--- betradar.js
+++ betradar.js
@@ -1,15 +1,21 @@
-const puppeteer = require('puppeteer');
-
-(async() => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
+var puppeteer = require('puppeteer'),
+	betradar = 'http://stats.betradar.com/s4/?clientid=1739&language=en#2_1,3_1,22_1,5_40942,9_headtohead,7_33,178_31';
 
 
-	await page.goto('http://stats.betradar.com/s4/?clientid=1739&language=en#2_1,3_1,22_1,5_40942,9_headtohead,7_33,178_31');
-	await page.waitFor('.percentagebar');
-	const contents = await page.content();
-
-	console.log(contents);
-
-	await browser.close();
-})();
+puppeteer.launch()
+	.then(function (browser) {
+		return browser.newPage()
+			.then(function (page) {
+				return page.goto(betradar)
+					.then(function () {
+						return page.waitFor('.percentagebar');
+					}).then(function () {
+						return page.content();
+					}).then(function (contents) {
+						console.log(contents);
+						return;
+					});
+			}).then(function () {
+				return browser.close();
+			});
+	});
---
+++ betradar_async.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+
+	await page.goto('http://stats.betradar.com/s4/?clientid=1739&language=en#2_1,3_1,22_1,5_40942,9_headtohead,7_33,178_31');
+	await page.waitFor('.percentagebar');
+	const contents = await page.content();
+
+	console.log(contents);
+
+	await browser.close();
+})();
