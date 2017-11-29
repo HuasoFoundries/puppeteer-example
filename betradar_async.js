9e757983fe974388ddb9270b4433f30dc9d1dbea
Felipe Figueroa <amenadiel@gmail.com>
2017-11-29 08:35:03 -0300

    betradar without async/await

--- betradar_async.js
+++ betradar_async.js
@@ -1,11 +1,12 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer'),
+	betradar = 'http://stats.betradar.com/s4/?clientid=1739&language=en#2_1,3_1,22_1,5_40942,9_headtohead,7_33,178_31';
 
 (async() => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
 
-	await page.goto('http://stats.betradar.com/s4/?clientid=1739&language=en#2_1,3_1,22_1,5_40942,9_headtohead,7_33,178_31');
+	await page.goto(betradar);
 	await page.waitFor('.percentagebar');
 	const contents = await page.content();
 
